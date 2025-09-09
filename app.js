0ccd1ae79bc32b51a366242ca2f138d45cdb1691
natefedida <natefedida@gmail.com>
2025-09-09 15:43:48 -0700

    Added server side validation with Joi

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const catchAsync = require('./utils/catchAsync')
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const morgan = require('morgan');
+const Joi = require('joi');
 const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground');
 const seedHelpers = require('./seeds/seedHelpers');
@@ -41,6 +43,21 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required()
+        }).required()
+    })
+    const { error } = campgroundSchema.validate(req.body);
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
@@ -69,8 +86,14 @@ app.delete('/campgrounds/:id', catchAsync(async(req, res) => {
     res.redirect('/campgrounds/')
 }))
 
+app.all(/(.*)/, (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
 app.use((err, req, res, next) => {
-    res.send('Oh boy, something went wrong!')
+    const {statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh no, Something went wrong!'
+    res.status(statusCode).render('error', { err });
 })
 
 app.listen(3000, () => {
