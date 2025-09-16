667b0251496ff92cc35b9282b6da1c42b1a2927c
natefedida <natefedida@gmail.com>
2025-09-15 22:12:58 -0700

    Added reviews functionality to campsites. Can upload and delete. Also added reviews as a db schema

--- models/campground.js
+++ models/campground.js
@@ -7,6 +7,12 @@ const CampgroundSchema = new Schema({
     image: String,
     description: String,
     price: Number,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
